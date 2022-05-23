5c0738278fe4020b8aa2a7a846067a2f47f1a89c
YanGCJ <YanGCJ@atguigu.com>
2022-05-23 20:38:30 +0800

    hot-fix commit

--- main.py
+++ main.py
@@ -14,5 +14,5 @@ if __name__ == '__main__':
     print_hi('PyCharm')
     print_hi('PyCharm')
     print_hi('PyCharm')
-
+    print_hi('hot-fix')
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
